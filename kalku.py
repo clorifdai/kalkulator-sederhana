22bbb118dc12182cdb9c4b5ed8aa16622619d36c
Arif <153439486+clorifdai@users.noreply.github.com>
2026-01-27 19:38:55 +0800

    Membuat fungsi pengurangan

--- kalku.py
+++ kalku.py
@@ -3,7 +3,14 @@ def tambah(depan: float, belakang: float):
     hasil = depan + belakang
     return hasil
 
+def kurang(depan: float, belakang: float):
+    """Pengurangan"""
+    hasil = depan - belakang
+    return hasil
 
 if __name__ == "__main__":
     plus = tambah(12, 16.9)
     print(plus)
+
+    kur = kurang(11, 100)
+    print(kur)
