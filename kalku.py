f583c46ebc67b1d4e40936917e51dd4c2d17b5c4
Arif <153439486+clorifdai@users.noreply.github.com>
2026-01-27 20:46:58 +0800

    Menyederhanakan fungsi perkalian, menghapus except. Kuhapus krn sudah bisa menangani except valueerror dgn cara lain

--- kalku.py
+++ kalku.py
@@ -11,17 +11,20 @@ def kurang(depan: float, belakang: float):
 def kali ():
     """Perkalian dengan input """
     list_angka = 1
+# Perlu menambah alat pendeteksi int dan str. 
+    while True:         
+        angka = input("Ketik q untuk keluar.\nMasukkan angka: ")
+        if angka == "q":
+            print("Anda keluar dari kalkulator")
+            break  
+        elif angka.isdigit():
+            print(angka, "sudah masuk")
+            list_angka *= int(angka)
+        else:
+            print("***")
+            print("Anda memasukkan bukan Angka")
+            print("***")
 
-    while True: 
-        try:
-                angka = int(input("Ketik -0 untuk keluar.\nMasukkan angka: "))
-                if angka == -0:
-                    print("Anda keluar dari kalkulator")
-                    break
-                print("Angka masuk", angka)
-                list_angka *= angka
-        except ValueError:
-            print("Masukkan angka bukan huruf maupun simbol!!")
     return list_angka            
 
 if __name__ == "__main__":
