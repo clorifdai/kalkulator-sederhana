58d54b247e5b0e1875a556bfe91d0302cfa559b5
Arif <153439486+clorifdai@users.noreply.github.com>
2026-01-27 20:06:10 +0800

    Membuat Fungsi perkalian dengan input, except

--- kalku.py
+++ kalku.py
@@ -8,9 +8,26 @@ def kurang(depan: float, belakang: float):
     hasil = depan - belakang
     return hasil
 
-if __name__ == "__main__":
-    plus = tambah(12, 16.9)
-    print(plus)
+def kali ():
+    """Perkalian dengan input """
+    list_angka = 1
 
-    kur = kurang(11, 100)
-    print(kur)
+    while True: 
+        try:
+                angka = int(input("Ketik -0 untuk keluar.\nMasukkan angka: "))
+                if angka == -0:
+                    print("Anda keluar dari kalkulator")
+                    break
+                print("Angka masuk", angka)
+                list_angka *= angka
+        except ValueError:
+            print("Masukkan angka bukan huruf maupun simbol!!")
+    return list_angka            
+
+if __name__ == "__main__":
+    # plus = tambah(12, 16.9)
+    # print(plus)
+
+    # kur = kurang(11, 100)
+    # print(kur)
+    print(kali())
