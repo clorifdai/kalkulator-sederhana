3fa2e9803f750983cc7ae35f50493f608f2782fe
Arif <153439486+clorifdai@users.noreply.github.com>
2026-01-30 18:36:39 +0800

    Membuat fungsi kuis penjumlahan sederhana, ini pakai modul random. Mau kutambahkan for/while loop agar bisa menghasilkan n kuis. Masih kurang di bagian membuat list/dictionary yang mengandung jawaban benar dan salah

--- kalku.py
+++ kalku.py
@@ -32,11 +32,33 @@ def bagi(depan: float, belakang:float):
     hasil = depan / belakang
     return hasil
 
+# Membuat tebakan nomer random
+def kuis_penambahan(level):
+    import random
+    "Level 1, Level 2, Level 3, level 4"
+    l1_depan = random.randint(0, 9)
+    l1_belakang = random.randint(0, 9)
+    # l2 = random.randint(10, 99)
+    # l3 = random.randint(100, 999)
+    # l4 = random.randint(1000, 9999)
+
+    if level == "l1":
+        print(l1_depan, "+", l1_belakang, "= ?")
+        hasil = l1_depan + l1_belakang
+        input_hasil = int(input("Masukkan angka: "))
+        if input_hasil == hasil:
+            return hasil
+        elif input_hasil != hasil:
+            return f"Hasil salah, yang benar {hasil}"
+
+
+
 if __name__ == "__main__":
+    print(kuis_penambahan("l1"))
     # plus = tambah(12, 16.9)
     # print(plus)
 
     # kur = kurang(11, 100)
     # print(kur)
     # print(kali())
-    print(bagi(12, 9))
+    # print(bagi(12, 9))
